5a192808812e989ec9d1e3cdc5aa8d823a1790bc
deepak9013216128 <deepak9013216128@gmail.com>
2020-08-25 15:03:22 +0530

    create express setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const exress = require('express');
+const path = require('path');
+
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
+const app = exress();
+
+app.use(exress.urlencoded({ extended: false }));
+app.use(exress.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoute);
+app.use(shopRoute);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+app.listen(3000, () => console.log('server is listening on port 3000'));
---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path')
+
+const rootDir = require('../utils/path');
+
+const router = express();
+
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+})
+
+module.exports = router;
