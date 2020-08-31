0f4d5987e088e7877b7add162938fbaf11cb2bb5
deepak9013216128 <deepak9013216128@gmail.com>
2020-08-31 21:03:34 +0530

    implement the editing route for editing product page

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,9 +5,29 @@ const Product = require('../models/products');
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    editing: false,
+  })
+}
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/')
+  }
+  const { productId } = req.params;
+  Product.findById(productId, product => {
+    if (!product) {
+      return res.redirect('/')
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product
+    })
   })
 }
 
